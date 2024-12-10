8698636d1a8c435a8634960fd90eee101616360f
green_tip <greentipjk@gmail.com>
2024-12-10 20:32:05 +0300

    added math + index

---
+++ math.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function miltiply(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
